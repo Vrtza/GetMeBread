59e7b099798e59af5a1754942fe1016c37869ded
AnibalDev <anibalrodriguezdeveloper@gmail.com>
2024-06-24 23:14:38 -0600

    Update reset password

--- app/src/main/java/com/bakerydev/breadlegram/components/ResetPassword.kt
+++ app/src/main/java/com/bakerydev/breadlegram/components/ResetPassword.kt
@@ -2,16 +2,32 @@ package com.bakerydev.breadlegram.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bakerydev.breadlegram.ui.theme.BreadlegramTheme
 
 private fun _resetPasswordButton() {
 
@@ -22,24 +38,39 @@ fun ResetPassword() {
     var email = remember { mutableStateOf("") }
     var password = remember { mutableStateOf("") }
     var repeatPassword = remember { mutableStateOf("") }
-    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-        Text("Resetear Contraseña", fontSize = 30.sp)
-        CustomTextField(text = "Email", value = email)
-        CustomTextField(text = "Password", value = password)
-        CustomTextField(text = "Confirmar contraseña", value = repeatPassword)
-        Button(onClick = { _resetPasswordButton()}) {
-            Text("Confirmar")
+    Surface(
+        modifier = Modifier.padding(8.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text("Resetear Contraseña", fontSize = 30.sp)
+            CustomTextField(text = "Email", value = email)
+            CustomTextField(text = "Password", value = password)
+            CustomTextField(text = "Confirmar contraseña", value = repeatPassword)
+            Button(
+                onClick = { _resetPasswordButton() }, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 8.dp)
+            ) {
+                Text("Confirmar")
+            }
         }
     }
 }
 
 @Composable
 fun CustomTextField(text: String, value: MutableState<String>) {
-    OutlinedTextField(value = value.value, onValueChange = {  value.value = it}, label = { Text(text) } )
+    TextField(value = value.value, onValueChange = {  value.value = it}, label = { Text(text) } )
 }
 
-@Preview()
+@Preview(showSystemUi = true)
 @Composable
 fun ResetPasswordPreview() {
-    ResetPassword()
+    BreadlegramTheme {
+        ResetPassword()
+    }
 }
--- app/src/main/java/com/bakerydev/breadlegram/ui/theme/Theme.kt
+++ app/src/main/java/com/bakerydev/breadlegram/ui/theme/Theme.kt
@@ -9,16 +9,23 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Color(0xFFFFFFFF),
     secondary = PurpleGrey80,
+//    urface = Color(0xFFFFFBFE),
+    surface = Color(0xFF000000),
     tertiary = Pink80
+
+//    val Purple80 = Color(0xFFD0BCFF)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Color(0xFFFFFFFF),
+    surface = Color(0xFF000000),
+//    primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
